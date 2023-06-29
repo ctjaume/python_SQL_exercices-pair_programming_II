ba09ba7621116e1049944f10d93d3d8b69b79631
ctjaume <ctjaumemiquel@gmail.com>
2023-06-29 12:56:25 +0200

    pair sql 29062023

--- sql/script_clase_29062023.sql
+++ sql/script_clase_29062023.sql
@@ -3,13 +3,21 @@ En esta lección de pair programming vamos a continuar trabajando sobre la base 
 El día de hoy vamos a realizar ejercicios en los que practicaremos sentencias SQL 
 usando el operador GROUP BY, que nos ayudará a agrupar o seleccionar los resultados según los valores 
 de algunos de los atributos de la tabla, y HAVING y CASE para añadir condiciones a la ejecución de 
-ciertas partes de las consultas de manera similar a como hacemos con WHERE (aunque con diferencias).
+ciertas partes de las consultas de manera similar a como hacemos con WHERE (aunque con diferencias).*/
 
-Ejercicios
+USE northwind;
+
+/*Ejercicios
 
 1.Relación entre número de pedidos y máxima carga:
-Desde logística nos piden el número de pedidos y la máxima cantidad de carga de entre los mismos (freight) 
+Desde logística nos piden el número de pedidos y la máxima cantidad de 
+carga de entre los mismos (freight) 
 que han sido enviados por cada empleado (mostrando el ID de empleado en cada caso).*/
+
+SELECT employee_id, COUNT(order_id) AS NumeroPedidos, MAX(freight) AS MaxCarga
+FROM orders
+GROUP BY employee_id;  -- agrupa lo que le hemos pedido por cada empleado
+
 
 /*2.Descartar pedidos sin fecha y ordénalos:
 Una vez han revisado los datos de la consulta anterior, nos han pedido afinar un poco más el "disparo". 
@@ -17,23 +25,49 @@ En el resultado anterior se han incluido muchos pedidos cuya fecha de envío est
 tenemos que mejorar la consulta en este aspecto. También nos piden que ordenemos los resultados 
 según el ID de empleado para que la visualización sea más sencilla.*/
 
+SELECT employee_id, COUNT(order_id) AS NumeroPedidos, MAX(freight) AS MaxCarga
+FROM orders
+WHERE shipped_date is NOT NULL
+GROUP BY employee_id; 
+    
+
 /*3.Números de pedidos por día:
 El siguiente paso en el análisis de los pedidos va a consistir en conocer mejor la distribución 
 de los mismos según las fechas. Por lo tanto, tendremos que generar una consulta que nos saque 
 el número de pedidos para cada día, mostrando de manera separada 
 el día (DAY()), el mes (MONTH()) y el año (YEAR()).*/
 
+SELECT COUNT(order_id), DAY(order_date), MONTH(order_date), YEAR(order_date)
+FROM orders
+GROUP BY order_date;
+
 /*4.Número de pedidos por mes y año:
 La consulta anterior nos muestra el número de pedidos para cada día concreto, 
 pero esto es demasiado detalle. Genera una modificación de la consulta anterior 
 para que agrupe los pedidos por cada mes concreto de cada año.*/
 
+SELECT COUNT(order_id), MONTH(order_date), YEAR(order_date)
+FROM orders
+GROUP BY MONTH(order_date);
 
 /*5.Seleccionad las ciudades con 4 o más empleadas:
 Desde recursos humanos nos piden seleccionar los nombres de las ciudades 
-con 4 o más empleadas de cara a estudiar la apertura de nuevas oficinas.
+con 4 o más empleadas de cara a estudiar la apertura de nuevas oficinas.*/
+
+SELECT COUNT(employee_id) AS NumeroEmpleadas, city AS Ciudad
+FROM employees
+GROUP BY city
+HAVING (NumeroEmpleadas) >= 4;
 
 /*6.Cread una nueva columna basándonos en la cantidad monetaria:
 Necesitamos una consulta que clasifique los pedidos en dos categorías ("Alto" y "Bajo") 
 en función de la cantidad monetaria total 
 que han supuesto: por encima o por debajo de 2000 euros.*/
+   
+SELECT unit_price * quantity AS TotalFactura,
+CASE 
+	WHEN unit_price * quantity >= 2000 THEN 'Alto'    
+    ELSE 'Bajo'
+    END AS 'Rendimiento'
+    FROM order_details;
+    
