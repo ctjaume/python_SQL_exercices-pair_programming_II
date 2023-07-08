415cc51fd7eae5285e46cb20ba4ebd3c58029774
ctjaume <ctjaumemiquel@gmail.com>
2023-07-08 13:11:59 +0200

    creo archivos pair prog 10072023

--- sql/script_clase_06072023.sql
+++ sql/script_clase_06072023.sql
@@ -1,9 +1,17 @@
+
+USE northwind;
+
 /*1. Extraed información de los productos "Beverages"
 En este caso nuestro jefe nos pide que le devolvamos toda la información necesaria para identificar un tipo de producto. En concreto, tienen especial interés por los productos con categoría "Beverages". Devuelve el ID del producto, el nombre del producto y su ID de categoría.
 La query debería resultar en una tabla como esta:
 ProductID / ProductName / CategoryID */
 
 
+SELECT *
+	FROM products
+    WHERE category_id IN (SELECT category_id
+								FROM categories
+								WHERE category_name = 'beverages');
 
 
 /* 2.Extraed la lista de países donde viven los clientes, pero no hay ningún proveedor ubicado en ese país
@@ -11,15 +19,21 @@ Suponemos que si se trata de ofrecer un mejor tiempo de entrega a los clientes, 
 Los resultados de esta query son:
 country / */
 
-
-
+SELECT DISTINCT country
+	FROM customers
+	WHERE country NOT IN (SELECT DISTINCT country 
+								FROM suppliers);
 
 /* 3.Extraer los clientes que compraron mas de 20 articulos "Grandma's Boysenberry Spread"
 Extraed el OrderId y el nombre del cliente que pidieron más de 20 artículos del producto "Grandma's Boysenberry Spread" (ProductID 6) en un solo pedido.
 Resultado de nuestra query deberíamos tener una tabla como esta:
 OrderID /CustomerID */
 
-
+SELECT customer_id, order_id
+	FROM orders    
+    WHERE order_id IN (SELECT order_id
+							FROM order_details
+							WHERE product_id = 6 AND quantity > 20);
 
 
 /* 4.Extraed los 10 productos más caros
@@ -27,14 +41,26 @@ Nos siguen pidiendo más queries correlacionadas. En este caso queremos saber cu
 Los resultados esperados de esta query son:
 Ten_most_expensive_products / UnitPrice */
 
-
-
+SELECT product_name, unit_price
+	FROM ( SELECT *
+			FROM products
+			ORDER BY unit_price DESC)  AS MiniTabla 
+	LIMIT 10;
 
 /* 5. BONUS:
 Qué producto es más popular
 Extraed cuál es el producto que más ha sido comprado y la cantidad que se compró.
 El resultado de esta query es:
-ProductNamr / MAX(SUmQUantity)
+ProductNamr / MAX(SUmQUantity)*/
 
+SELECT *
+FROM order_details;
+
+SELECT product_id, SUM(quantity) AS CantidadTotal
+	FROM order_details
+    GROUP BY product_id
+    HAVING SUM(quantity) >= ALL (SELECT SUM(quantity)
+	FROM order_details)
+    
 
 
---
+++ sql/script_clase_10072023.sql
@@ -0,0 +1,41 @@
+/*1.Extraer en una CTE todos los nombres de las compañias y los id de los clientes.
+Para empezar nos han mandado hacer una CTE muy sencilla el id del cliente y el nombre de la compañia de la tabla Customers.
+Los resultados de esta query serán:
+CustomerID / CompanyName*/
+
+
+
+
+/*2.Selecciona solo los de que vengan de "Germany"
+Ampliemos un poco la query anterior. En este caso, queremos un resultado similar al anterior, pero solo queremos los que pertezcan a "Germany".
+Los resultados de esta query serán:
+-CustomerID / CompanyName*/
+
+
+
+
+
+/*3.Extraed el id de las facturas y su fecha de cada cliente.
+En este caso queremos extraer todas las facturas que se han emitido a un cliente, su fecha la compañia a la que pertenece.
+📌 NOTA En este caso tendremos columnas con elementos repetidos(CustomerID, y Company Name).
+Los resultados de esta query serán:
+CustomerID / CompanyName / OrderID / OrderDate*/
+
+
+
+
+/*4. Contad el número de facturas por cliente
+Mejoremos la query anterior. En este caso queremos saber el número de facturas emitidas por cada cliente.
+Los resultados de esta query serán:
+CustomerID / CompanyName / NumeroFactura*/
+
+
+
+
+/*5.Cuál la cantidad media pedida de todos los productos ProductID.
+Necesitaréis extraer la suma de las cantidades por cada producto y calcular la media.
+Los resultados de esta query serán:
+Producto / Media */
+
+
+
